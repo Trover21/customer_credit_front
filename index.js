a0cbf141e9a0cacd25fd291bec8dc947e30dd7d8
kidu girum <girumkidu@gmail.com>
2026-06-12 14:38:01 +0300

    added dotenv setup

--- index.js
+++ index.js
@@ -60,16 +60,15 @@ const Customer = mongoose.model('Customer', customerSchema);
 /* ============================
    MongoDB Connection
 ============================ */
-const uri = process.env.MONGO_URI ||
-"mongodb+srv://zemakidoo2015_db_user:MiracleTrove2018@customercredit.odinemv.mongodb.net/customerDB?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri)
-.then(() => {
-  console.log("MongoDB Connected Successfully!");
-})
-.catch((err) => {
-  console.log("MongoDB Error:", err);
-});
+  .then(() => {
+    console.log("MongoDB Connected Successfully!");
+  })
+  .catch((err) => {
+    console.log("MongoDB Error:", err);
+  });
 
 /* ============================
    1. ADD CUSTOMER
