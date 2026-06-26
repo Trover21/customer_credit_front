eb7e4f269d49463d1a306706224cd9de280d8f0c
kidu girum <girumkidu@gmail.com>
2026-06-26 14:08:39 +0300

    chore: setup gitignore, add app_strings and package-lock

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,13 @@
+// core/constants/app_strings.dart
+class AppStrings {
+  static const String appName = "Firest Flutter Project";
+  static const String welcomeBack = "Welcome To Our Company!!!";
+  static const String signIn = "Sign In";
+  static const String signUp = "Sign Up";
+  static const String forgotPassword = "Forgot Password?";
+  static const String emailPlaceholder = "Enter your email";
+  static const String passwordPlaceholder = "Enter your password";
+  static const String usernamePlaceholder = "Enter your username";
+  static const String loginError = "Invalid credentials, please try again.";
+  static const String signupSuccess = "Account created successfully! Please verify your email.";
+}
